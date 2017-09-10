e3e5e8fe9d50d6aa9e68d5b93d8e532a377f09f9
wolf1996 <ksgiv37@gmail.com>
2017-09-10 20:48:35 +0300

    Травис, жги

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World")
 }
 
+// коммент чтоб было что коммитить
 func main(){
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", helloWorldHandler)
